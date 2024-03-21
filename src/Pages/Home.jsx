b6ad363c22af7fdde291177474931e415a8eb4b5
rayhanbeg <mdrayhan40301@gmail.com>
2024-03-21 19:32:48 +0600

    first commit with navbar

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav/>
+            <h1>HomePage</h1>
+        </div>
+    );
+};
+
+export default Home;
